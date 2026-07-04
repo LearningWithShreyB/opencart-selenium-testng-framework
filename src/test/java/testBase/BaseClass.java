e0bdb852675021c607f5bf6957a05aa1c3b734f6
ShreyB <shreypractice.github@gmail.com>
2026-07-04 17:52:02 +0530

    Updates_0407_17:52

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,12 +1,14 @@
 package testBase;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -17,28 +19,35 @@ public class BaseClass {
 
 	public WebDriver driver;
 	public Logger logger;
+	public Properties p;
 
 	@BeforeClass
 	@Parameters({ "os", "browser" })
-	public void setup(String os, String br) {
+	public void setup(String os, String br) throws IOException {
 
-	    logger = LogManager.getLogger(this.getClass());
+		// loading properties file
+		FileReader file = new FileReader(".//src//test//resources//config.properties");
+		p = new Properties();
+		p.load(file);
 
-	    switch (br.toLowerCase()) {
-	    case "chrome":
-	        driver = new ChromeDriver();
-	        break;
-	    case "edge":
-	        driver = new EdgeDriver();
-	        break;
-	    default:
-	        throw new IllegalArgumentException("Invalid browser: " + br);
-	    }
+		// loading log4j file
+		logger = LogManager.getLogger(this.getClass());
 
-	    driver.manage().deleteAllCookies();
-	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	    driver.get("https://tutorialsninja.com/demo/");
-	    driver.manage().window().maximize();
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		default:
+			throw new IllegalArgumentException("Invalid browser: " + br);
+		}
+
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
 	}
 
 	@AfterClass
