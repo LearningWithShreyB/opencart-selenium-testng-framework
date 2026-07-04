24851b788553bfc7a55971be7e7cf8659556f494
ShreyB <shreypractice.github@gmail.com>
2026-07-04 17:07:36 +0530

    Updates_0407_17:08

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -5,55 +5,61 @@ import java.time.Duration;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.Parameters;
 import org.apache.logging.log4j.LogManager;//log4j
-import org.apache.logging.log4j.Logger;   //log4j
+import org.apache.logging.log4j.Logger; //log4j
 
 public class BaseClass {
 
 	public WebDriver driver;
 	public Logger logger;
-	
+
 	@BeforeClass
-	public void setup()
-	{
-		
-		logger=LogManager.getLogger(this.getClass());//Log4j
-		
-		driver=new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.get("https://tutorialsninja.com/demo/");
-		driver.manage().window().maximize();
+	@Parameters({ "os", "browser" })
+	public void setup(String os, String br) {
+
+	    logger = LogManager.getLogger(this.getClass());
+
+	    switch (br.toLowerCase()) {
+	    case "chrome":
+	        driver = new ChromeDriver();
+	        break;
+	    case "edge":
+	        driver = new EdgeDriver();
+	        break;
+	    default:
+	        throw new IllegalArgumentException("Invalid browser: " + br);
+	    }
+
+	    driver.manage().deleteAllCookies();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    driver.get("https://tutorialsninja.com/demo/");
+	    driver.manage().window().maximize();
 	}
-	
+
 	@AfterClass
-	public void tearDown()
-	{
+	public void tearDown() {
 		driver.close();
 	}
-	
 
-	public String randomString()
-	{
-		String generatedString=RandomStringUtils.randomAlphabetic(5);
+	public String randomString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
 		return generatedString;
 	}
-	
-	public String randomNumber()
-	{
-		String generatedString=RandomStringUtils.randomNumeric(10);
+
+	public String randomNumber() {
+		String generatedString = RandomStringUtils.randomNumeric(10);
 		return generatedString;
 	}
-	
-	public String randomAlphaNumeric()
-	{
-		String str=RandomStringUtils.randomAlphabetic(3);
-		String num=RandomStringUtils.randomNumeric(3);
-		
-		return (str+"@"+num);
+
+	public String randomAlphaNumeric() {
+		String str = RandomStringUtils.randomAlphabetic(3);
+		String num = RandomStringUtils.randomNumeric(3);
+
+		return (str + "@" + num);
 	}
 }
